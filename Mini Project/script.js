fe4c4a757596d44280ff8ffeeb67911b0c1ac5ed
Amna Nihal <amnanihal@gmail.com>
2024-05-15 01:02:19 +0500

    Mini project

---
+++ Mini Project/script.js
@@ -0,0 +1,20 @@
+
+// About
+
+function show(){
+    let section= document.getElementById('about');
+    section.scrollIntoView({behavior:"smooth"});
+}
+
+// End
+
+// -------------------------------------------------More------------------------------------------------
+function service(){
+    let sec2= document.getElementById('services');
+    sec2.scrollIntoView({behavior: "smooth"});
+}
+// End
+function info(){
+    let a= document.getElementById('effect');
+    a.scrollIntoView({behavior:"smooth"});
+}
